7950eefda58ae7b9e18c82fc1c9e3fe8c6e2a6b9
Tyago03 <146128433+Tyago03@users.noreply.github.com>
2024-06-04 15:46:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import requests
+from bs4 import BeautifulSoup
 
 app = FastAPI()
 
@@ -15,10 +16,6 @@ app.add_middleware(
 @app.get("/hello")
 def read_root():
     return {"msg": "Hello World!"}
-
-@app.get("/hello/{nome}")
-def hello(nome: str):
-    return {"msg": f"Hello {nome}!"}
 
 @app.get("/cep/{cep}")
 def get_cep_info(cep: str):
@@ -36,3 +33,25 @@ def get_cep_info(cep: str):
         "uf": data.get("uf", "")
     }
     return filtered_data
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+}
+
+@app.get("/ticker/{codigo_ticker}")
+def get_ticker_info(codigo_ticker: str):
+    url = f"https://www.fundamentus.com.br/detalhes.php?papel={codigo_ticker.upper()}"
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    td = soup.find('td', class_='data destaque w3')
+    if td:
+        span = td.find('span', class_='txt')
+        if span:
+            cotacao = span.text.strip()
+            return {
+                "ticker": codigo_ticker.upper(),
+                "cotacao": float(cotacao.replace(',', '.'))
+            }
+    return {"error": "Cotação não encontrada"}
+
